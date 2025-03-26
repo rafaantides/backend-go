c5c9bac871d8e080c024b986e202d5946533de67
RafaelAntides <rafaantides@gmail.com>
2025-03-26 17:42:54 -0300

    uuid_validator

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,9 +1,6 @@
 package utils
 
 import (
-	"backend-go/internal/api/errs"
-	"reflect"
-
 	"github.com/google/uuid"
 )
 
@@ -17,39 +14,4 @@ func ToUUIDPointer(str string) (*uuid.UUID, error) {
 		return nil, err
 	}
 	return &parsedUUID, nil
-}
-
-func ValidateUUIDs(strings []string) error {
-	for _, str := range strings {
-		if _, err := uuid.Parse(str); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func ValidateUUIDArrayFields(s any) *string {
-	val := reflect.ValueOf(s).Elem()
-
-	// Percorre todos os campos da struct
-	for i := range val.NumField() {
-		field := val.Type().Field(i)
-		fieldValue := val.Field(i)
-
-		// Verifica se o campo é um ponteiro para slice de strings
-		if fieldValue.Kind() == reflect.Ptr && !fieldValue.IsNil() && fieldValue.Elem().Kind() == reflect.Slice {
-			slice := fieldValue.Elem()
-
-			// Valida apenas se o slice não estiver vazio
-			if slice.Len() > 0 {
-				sliceValue := slice.Interface().([]string)
-				if err := ValidateUUIDs(sliceValue); err != nil {
-					detail := errs.InvalidUUID(field.Tag.Get("form"), err.Error())
-					return &detail
-				}
-			}
-		}
-	}
-
-	return nil
 }
